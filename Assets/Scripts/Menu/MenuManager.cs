199fbb9e0a79b0c80a395410f41ce31adc22ef31
Czkerim <v-mirek@centrum.cz>
2020-11-06 22:59:15 +0100

    Dodelane_zakladni_menu

--- Assets/Scripts/Menu/MenuManager.cs
+++ Assets/Scripts/Menu/MenuManager.cs
@@ -3,36 +3,79 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
-    public TextMeshProUGUI text_novahra;
+    public TextMeshProUGUI text_Newgame;
     public TextMeshProUGUI text_pressButton;
-  
+    public GameObject warning_text;
+    float posX;
+    float posY;
+    int clickNumber;
+
     public List<Image> button;
-    protected bool prvnistart;
+    public List<TextMeshProUGUI> button_text;
+    protected bool firstStart;
  
     void Start()
     {
-        prvnistart = true;
+        firstStart = true;
 
         for (int i = 0; i < 3; i++)
         {
             button[i].color = new Color(button[i].color.r, button[i].color.g, button[i].color.b, 0);
+            button_text[i].alpha = 0;
         }
     }
 
-  void Update()
+   public void Button_start()
+    {
+        button[0].color = Color.Lerp(Color.white, Color.clear, 1);
+        button_text[0].alpha = 0;
+    }
+    public void Button_Option_click()
+    {
+        SceneManager.LoadScene("MainGame");
+    }
+    public void Button_Option_pointer()
+    {
+        warning_text.SetActive(true);
+    }
+    public void Button_Option_pointer_exit()
+    {
+        warning_text.SetActive(false);
+
+    }
+    public void Button_Quit()
+    {
+       
+        generator();
+        button[2].transform.localPosition = Vector3.Lerp(button[2].transform.localPosition, new Vector3(posX, posY,0),1);
+        clickNumber++;
+        if (clickNumber >=5)
+        {
+            Application.Quit();
+        }
+    }
+
+    void Update()
   {
     if (Input.anyKeyDown)
     {
-      if (Input.anyKeyDown && prvnistart)
+      if (Input.anyKeyDown && firstStart)
       {
         StartCoroutine(Fade());
       }
     }
   }
+    void generator()
+    {
+        posX = Random.Range(-649, 470);
+        posY = Random.Range(-301, 362);
+        Debug.Log("posX" + posX + "posY" + posY);
+    }
         
     IEnumerator Fade()
     {
@@ -52,7 +95,7 @@ public class MenuManager : MonoBehaviour
         {
             currentValue -= Time.deltaTime;
         
-            text_novahra.alpha = currentValue;
+            text_Newgame.alpha = currentValue;
             Debug.LogError("probehlo");
             text_pressButton.alpha = currentValue;
             yield return null;
@@ -72,11 +115,12 @@ public class MenuManager : MonoBehaviour
             for (int i = 0; i < 3; i++)
             {
                 button[i].color = new Color(button[i].color.r, button[i].color.g, button[i].color.b, currentValue);
-
+                button_text[i].alpha = currentValue;
             }
             yield return null;
         }
-        prvnistart = false;
+        firstStart = false;
     }
+
 
 }
---
+++ Assets/Scripts/tools/Gyzmo.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gyzmo : MonoBehaviour {
+
+
+
+    private void OnDrawGizmos()//
+    {
+        Gizmos.color = Color.blue;        
+              
+        Gizmos.DrawSphere(transform.position + transform.forward * 45f, 45f);
+    }
+}
