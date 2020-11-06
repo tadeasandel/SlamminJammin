921b448ed0760cbbe3bf8a9b3e9212ce961c08dc
Czkerim <v-mirek@centrum.cz>
2020-11-06 19:44:00 +0100

    Implementace časovače

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public float timeRemaining;
+    public float maxTimeEnding;
+    float deathTime;
+    public Text timerText;
+    // Start is called before the first frame update
+     void Start()
+    {
+        deathTime = Random.Range(0, maxTimeEnding);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        if (timeRemaining > deathTime)
+        {
+            timeRemaining -= Time.deltaTime;
+           
+
+            DisplayTime(timeRemaining);
+        }
+        else
+        {
+            Debug.LogError("You are death");
+
+        }
+
+        
+    }
+    void DisplayTime(float timeToDisplay) 
+    {
+       
+            float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+            float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+
+        
+
+    }
+}
