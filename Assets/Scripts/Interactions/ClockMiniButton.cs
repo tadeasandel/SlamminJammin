b94c4dbc062d295b157454a43c934334b2b583ef
Czkerim <v-mirek@centrum.cz>
2020-11-07 15:46:54 +0100

    -Opravasipek

--- Assets/Scripts/Interactions/ClockMiniButton.cs
+++ Assets/Scripts/Interactions/ClockMiniButton.cs
@@ -4,8 +4,34 @@ using UnityEngine;
 
 public class ClockMiniButton : MonoBehaviour
 {
+    public GameObject Sipka;
+    public float RotationSpeed;
+    public float Distance;
+
+
+    void Update()
+    {
+        if (!ispressed)
+            return;
+         Sipka.transform.Rotate(0, 0, (Distance * RotationSpeed * Time.deltaTime), Space.World);
+        Debug.Log("Pressed");
+    }
+    bool ispressed = false;
+    public void OnPointerDown()
+    {
+        ispressed = true;
+    }
+
+    public void OnPointerUp()
+    {
+        ispressed = false;
+    }
+
+
+
     public void Doleva()
     {
         Debug.LogWarning("Doleve");
+        Sipka.transform.Rotate(0, 0, (Distance * RotationSpeed * Time.deltaTime), Space.World);
     }
 }
