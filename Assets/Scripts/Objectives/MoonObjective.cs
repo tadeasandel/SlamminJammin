6736be433281011c7f59f7966f9a2032c0905d5a
Tadeáš Anděl <tadeas.andel@gmail.com>
2020-11-08 15:09:23 +0100

    side objective finished

--- Assets/Scripts/Objectives/MoonObjective.cs
+++ Assets/Scripts/Objectives/MoonObjective.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class MoonObjective : MainObjectiveBase
 {
+  public RecorderSideObjective sideObjective;
+
+  public override void InitObjective(ObjectivesManager objectivesManager)
+  {
+    base.InitObjective(objectivesManager);
+    sideObjective.InitObjective(this);
+  }
+
   public override void FinishObjective()
   {
-    // fly to moon animation + SFX
     base.FinishObjective();
   }
 }
